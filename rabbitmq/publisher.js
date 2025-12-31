ef6f30df73af9bad68bf3b40d9fd04cdd5fc523d
dev-rfgul <rfgul587@gmail.com>
2025-12-31 17:34:37 +0500

    feat: implement RabbitMQ consumer and publisher for job processing

---
+++ rabbitmq/publisher.js
@@ -0,0 +1,23 @@
+/* RabbitMQ */
+const amqp = require("amqplib");
+
+const msg = {number: process.argv[2]}
+connect();
+async function connect() {
+
+    try {
+        // const amqpServer = "amqp://localhost:5672"
+        const amqpServer = "amqps://lgsufjaa:a6mumSenq2jbHFGUpkYBI95Vq1GtXJ1f@duck.lmq.cloudamqp.com/lgsufjaa"
+        const connection = await amqp.connect(amqpServer)
+        const channel = await connection.createChannel();
+        await channel.assertQueue("jobs");
+        await channel.sendToQueue("jobs", Buffer.from(JSON.stringify(msg)))
+        console.log(`Job sent successfully ${msg.number}`);
+        await channel.close();
+        await connection.close();
+    }
+    catch (ex){
+        console.error(ex)
+    }
+
+}
