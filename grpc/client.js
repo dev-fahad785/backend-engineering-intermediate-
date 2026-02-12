10d173e74b43e3062a1267f28972df824a366637
dev-rfgul <rfgul587@gmail.com>
2026-02-12 18:52:43 +0500

    refactor: migrate from grpc to @grpc/grpc-js and update server implementation
    - Updated package.json to replace 'grpc' with '@grpc/grpc-js'.
    - Refactored server.js to use bindAsync for server binding and added console logging for server start.
    - Implemented a basic readTodos function to avoid client hanging.
    - Added a new client.js file to demonstrate creating a Todo item.

---
+++ grpc/client.js
@@ -0,0 +1,18 @@
+const grpc=require('grpc');
+const protoLoader=require('@grpc/proto-loader');
+const packageDef=protoLoader.loadSync('todo.proto',{})
+const grpcObject=grpc.loadPackageDefinition(packageDef);
+const todoPackage=grpcObject.todoPackage; 
+
+const client=new todoPackage.Todo('localhost:40000', grpc.credentials.createInsecure());
+
+client.createTodo(
+    {
+        "id":-1,
+        "text":"Learn gRPC"
+    },
+    (err, response) => {
+        if(err) console.error(err);
+        else console.log('Created Todo:', response);
+    }  
+)
--- grpc/server.js
+++ grpc/server.js
@@ -1,19 +1,41 @@
-const grpc=require('grpc');
-const protoLoader=require('@grpc/proto-loader');
-const packageDef=protoLoader.loadSync('todo.proto',{})
-const grpcObject=grpc.loadPackageDefinition(packageDef);
-const todoPackage=grpcObject.todoPackage; 
+const grpc = require('@grpc/grpc-js'); // Changed from 'grpc'
+const protoLoader = require('@grpc/proto-loader');
 
-const server=new grpc.Server()
-server.bind('0.0.0.0:40000',grpc.ServerCredentials.createInsecure());
-server.addService(todoPackage.Todo.service,{
-    "createTodo":createTodo,
-    "readTodos":readTodos,
-})
+const packageDef = protoLoader.loadSync('todo.proto', {});
+const grpcObject = grpc.loadPackageDefinition(packageDef);
+const todoPackage = grpcObject.todoPackage;
 
-server.start();
-function createTodo(call, callback){
+const server = new grpc.Server();
+
+// In the new library, bindAsync is used instead of bind
+server.bindAsync(
+    '0.0.0.0:40000', 
+    grpc.ServerCredentials.createInsecure(), 
+    (error, port) => {
+        if (error) {
+            console.error(error);
+            return;
+        }
+        console.log(`Server running at http://0.0.0.0:${port}`);
+        // .start() is now implicit in bindAsync or called here
+    }
+);
+
+server.addService(todoPackage.Todo.service, {
+    "createTodo": createTodo,
+    "readTodos": readTodos,
+});
+
+function createTodo(call, callback) {
     console.log('Received from client: ' + call.request.text);
-    callback(null, {id: 1, text: call.request.text})
+    const todoItem = {
+        id: 1, // In a real app, you'd increment this
+        text: call.request.text
+    };
+    callback(null, todoItem);
 }
-function readTodos(call, callback){}
+
+function readTodos(call, callback) {
+    // Basic placeholder implementation to avoid hanging the client
+    callback(null, { items: [] });
+}
